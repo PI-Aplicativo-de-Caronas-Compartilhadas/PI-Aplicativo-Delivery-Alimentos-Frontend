014c89740fb5cd5e8c210cf5501e33b982757086
lilianlacerda <lilianraiany@gmail.com>
2026-07-13 16:04:13 -0300

    implementação dos alertas personalizados

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,42 @@ import "./index.css";
 import ListaCategorias from "./components/categoria/listacategorias/ListaCategorias";
 import FormCategoria from "./components/categoria/formcategoria/FormCategoria";
 import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* Altere bg-[#022110] para bg-white aqui embaixo */}
-      <div className="flex flex-col min-h-screen bg-white text-[#042f17]">
-        <Navbar />
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        {/* Altere bg-[#022110] para bg-white aqui embaixo */}
+        <div className="flex flex-col min-h-screen bg-white text-[#042f17]">
+          <Navbar />
 
-        <div className="flex-1">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/categoria" element={<ListaCategorias />} />
-            <Route
-              path="/produtos"
-              element={
-                <h1 className="text-3xl font-bold p-8">Página de Produtos</h1>
-              }
-            />
-            <Route path="/cadastrarcategoria" element={<FormCategoria />} />
-            <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-            <Route
-              path="/deletarcategoria/:id"
-              element={<DeletarCategoria />}
-            />
-          </Routes>
+          <div className="flex-1">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/categoria" element={<ListaCategorias />} />
+              <Route
+                path="/produtos"
+                element={
+                  <h1 className="text-3xl font-bold p-8">Página de Produtos</h1>
+                }
+              />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route
+                path="/deletarcategoria/:id"
+                element={<DeletarCategoria />}
+              />
+            </Routes>
+          </div>
+
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
